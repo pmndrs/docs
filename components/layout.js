9fadd9110768316d5acd2b0e11691d0d1551f193
Paul Henschel <drcmda@gmail.com>
2020-10-27 10:19:43 +0100

    add video

--- components/layout.js
+++ components/layout.js
@@ -3,14 +3,12 @@ import Footer from './footer';
 
 export default function Layout({ children, pageTitle, ...props }) {
   return (
-    <>
+    <div className="main">
       <Head>
         <title>{pageTitle}</title>
       </Head>
-
       <main>{children}</main>
-
       <Footer />
-    </>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import Layout from '../components/layout';
 export default function Index() {
   return (
     <>
+      <video className="video" playsInline autoPlay muted loop>
+        <source src="/bg.mp4" type="video/mp4" />
+      </video>
       <Layout pageTitle="Poimandres">
         <a href="https://github.com/pmndrs">
           <h1>Poimandres</h1>
         </a>
-        <div className="video"></div>
       </Layout>
     </>
   );
