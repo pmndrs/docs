71b87e6ea3a029f1c244a5dda6e0e262d64d337c
Paul Henschel <drcmda@gmail.com>
2021-03-22 16:22:33 +0100

    intro

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: '/react-three-fiber/getting-started/installation',
+        destination: '/react-three-fiber/getting-started/introduction',
         permanent: true,
       },
       {
