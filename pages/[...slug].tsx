0ca1575f72ee350f0b70f13b8f1410f63de75f8d
SaraVieira <hey@iamsaravieira.com>
2021-04-15 18:38:38 +0200

    add mobile

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -46,7 +46,7 @@ const components = {
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200"> {children}</table>
+              <table className="divide-y divide-gray-200"> {children}</table>
             </div>
           </div>
         </div>
