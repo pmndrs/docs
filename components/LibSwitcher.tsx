298b74dfc15ef2e441e8c9db9ee1f05b3a9251a0
SaraVieira <hey@iamsaravieira.com>
2021-04-19 16:08:08 +0200

    add start of drei

--- components/LibSwitcher.tsx
+++ components/LibSwitcher.tsx
@@ -1,12 +1,15 @@
+import { useRouter } from 'next/router'
+
 function LibSwitcherModal() {
   return null
 }
 
 export default function LibSwitcher() {
+  const { query } = useRouter()
   return (
     <>
-      <div className="pl-6 px-6 py-2 bg-blue-50 rounded-md text-gray-900 uppercase tracking-wide font-semibold text-sm lg:text-xs">
-        React Three Fiber
+      <div className="pl-6 px-6 py-2 bg-blue-50 rounded-md text-gray-900 uppercase tracking-wide font-semibold text-sm lg:text-xs text-capitalize">
+        {query.slug[0].split('-').join(' ')}
       </div>
     </>
   )
