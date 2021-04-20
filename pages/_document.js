96e035a179996bbfbb63a29e9800f6070eef85d9
SaraVieira <hey@iamsaravieira.com>
2021-04-20 22:10:31 +0200

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -14,11 +14,11 @@ export default class Doc extends Document {
           <meta name="robots" content="follow, index, noarchive" />
           <meta name="viewport" content="initial-scale=1,width=device-width" />
 
-          <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 
-          <link rel="manifest" href="./site.webmanifest" />
+          <link rel="manifest" href="/site.webmanifest" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="#000000" />
           <meta name="apple-mobile-web-app-title" content="PMNDRS" />
