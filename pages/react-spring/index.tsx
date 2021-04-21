d001fa2da7de1d5e380cd0bc0c8d42a2c1947b2f
Paul Henschel <drcmda@gmail.com>
2021-04-21 13:53:02 +0200

    replace redirect

--- pages/react-spring/index.tsx
+++ pages/react-spring/index.tsx
@@ -1,18 +1,3 @@
-export default function Index() {
-  return null
-}
+import redirect from '../../utils/redirect'
 
-async function getInitialProps({ res }) {
-  const targetURL = '/react-spring/introduction'
-  if (res) {
-    res.writeHead(307, { Location: targetURL })
-    res.end()
-  } else {
-    // @ts-ignore
-    window.location = targetURL
-    await new Promise((resolve) => {})
-  }
-  return
-}
-
-Index.getInitialProps = getInitialProps
+export default redirect('/react-spring/introduction')
