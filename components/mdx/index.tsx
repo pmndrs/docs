dc0f0f7eb5f15c4f6d82ed064db57991145bf736
Paul Henschel <drcmda@gmail.com>
2021-04-24 13:12:01 +0200

    fix hints

--- components/mdx/index.tsx
+++ components/mdx/index.tsx
@@ -25,7 +25,7 @@ const components = {
   Sandbox,
   TweetGrid,
   Hint: ({ children }) => (
-    <div className="shadow overflow-hidden bg-yellow-100 border-b border-gray-200 sm:rounded-lg px-6 py-4 leading-7 mb-6">
+    <div className="shadow overflow-hidden bg-yellow-100 border-b border-gray-200 sm:rounded-lg px-6 py-4 mb-6">
       {children}
     </div>
   ),
