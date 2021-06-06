f028f99c3d1cf13e3a175e4224bffa719ab2913d
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2021-06-06 18:08:02 -0400

    docs: UI/UX improvements (#84)
    * feat(jotai): revise docs for atomWithStorage util
    
    * build: update dependencies
    
    * feat: replace library switcher with traditional popover
    
    * fix: restore Zustand redirect behavior
    
    * chore: add consistent CSS declaration order for @font-face rules
    
    * fix: remove bugged capitalization
    
    * feat: add new index page
    
    * fix: remove overflow
    
    * fix: layout
    
    * feat: mobile optimizations
    
    * fix: restore fix

--- components/LibSwitcher.tsx
+++ components/LibSwitcher.tsx
@@ -1,139 +1,40 @@
-import clsx from 'clsx'
-import SimpleModal from 'components/Modal'
-import { useCallback, useEffect, useState } from 'react'
-import { animated as a, useTransition, useChain } from 'react-spring'
-import { switcherContentRef, switcherModalRef, useSwitcher } from 'store/switcher'
-import { useRouter } from 'next/router'
-import useKeyPress from 'hooks/useKeyPress'
-import { Close } from './Icons'
 import { useMemo } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import cx from 'clsx'
+import { Popover } from '@headlessui/react'
 
-const data = [
-  {
-    id: 'react-three-fiber',
-    label: 'React Three Fiber',
-  },
-  {
-    id: 'react-spring',
-    label: 'React Spring',
-  },
-  {
-    id: 'drei',
-    label: 'Drei',
-  },
-  {
-    id: 'zustand',
-    label: 'Zustand',
-  },
-  {
-    id: 'jotai',
-    label: 'Jotai',
-  },
-  {
-    id: 'a11y',
-    label: 'A11y',
-  },
-  {
-    id: 'react-postprocessing',
-    label: 'React Postprocessing',
-  },
-]
-
-function SwitcherContent({ open, handleClick }) {
-  const router = useRouter()
-
-  const _data = useMemo(() => data.filter(({ id }) => id !== router.query.slug[0]), [router])
-
-  const transitions = useTransition(open ? _data : [], (item) => item.id, {
-    ref: switcherContentRef,
-    unique: true,
-    trail: 100,
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-    config: {
-      tension: 280,
-      friction: 28,
-    },
-  })
-
-  return (
-    <div className="space-y-4 py-8 mx-4">
-      {transitions.map(({ item, props, key }) => (
-        <a.div
-          key={key}
-          style={props}
-          className={clsx(
-            'font-bold text-center text-2xl rounded-md p-16',
-            router.query.slug[0] === key
-              ? 'text-white bg-black hover:bg-black'
-              : 'font-bold cursor-pointer bg-gray-200 hover:bg-black hover:text-white'
-          )}
-          onClick={() => handleClick(item.id)}
-        >
-          {item.label}
-        </a.div>
-      ))}
-    </div>
-  )
-}
+import { data } from '../data/libraries'
 
 export default function LibSwitcher() {
-  const { isSwitcherOpen, setIsSwitcherOpen, toggleSwitcher } = useSwitcher()
-  const labelSizeClasses = 'block px-6 py-2'
-  const { query } = useRouter()
-  const escPressed = useKeyPress('Escape')
   const router = useRouter()
-
-  const handleClick = useCallback(
-    (path?: string) => {
-      toggleSwitcher()
-      if (path && router.query.slug[0] !== path) {
-        router.push(`/${path}`)
-      }
-    },
-    [router, toggleSwitcher]
+  const { query } = router
+  const currentPage = useMemo(
+    () => data.find((item) => item.id === query.slug[0]).label,
+    [data, query]
   )
-
-  useChain(
-    isSwitcherOpen
-      ? [switcherModalRef, switcherContentRef]
-      : [switcherContentRef, switcherModalRef],
-    [0, 0.35]
-  )
-
-  useEffect(() => {
-    if (escPressed) {
-      setIsSwitcherOpen(false)
-    }
-  }, [escPressed])
 
   return (
-    <>
-      <SimpleModal open={isSwitcherOpen}>
-        <div className="sticky top-0 left-0 right-0 grid grid-cols-3 bg-black text-white h-16">
-          <div className="flex items-center flex-none pl-4 border-b border-gray-200 sm:pl-6 xl:pl-8 lg:border-b-0 lg:w-60 xl:w-72">
-            <span className="font-bold cursor-pointer">Pmndrs</span>
-            <span className="font-normal cursor-pointer">.docs</span>
-          </div>
-          <div className="flex justify-center items-center font-semibold capitalize">
-            <span className="hidden md:inline">{query.slug[0].replace(/\-/g, ' ')}</span>
-          </div>
-          <div className="flex justify-end items-center h-16">
-            <Close className="m-4 w-8 h-8 cursor-pointer" onClick={() => handleClick()} />
-          </div>
+    <Popover className="relative">
+      <Popover.Button className="block w-full px-6 py-2 bg-black rounded-md text-white">
+        {currentPage}
+      </Popover.Button>
+      <Popover.Panel className="absolute z-10 w-full mt-4 p-3 shadow-xl bg-white rounded-md">
+        <div className="flex flex-col space-y-3">
+          {data.map((item) => (
+            <Link key={item.id} href={item.id}>
+              <a
+                className={cx(
+                  'px-3 py-2 hover:bg-gray-50 rounded-md font-normal',
+                  item.id === query.slug[0] && 'sr-only'
+                )}
+              >
+                {item.label}
+              </a>
+            </Link>
+          ))}
         </div>
-        <SwitcherContent open={isSwitcherOpen} handleClick={handleClick} />
-      </SimpleModal>
-      <div
-        className={clsx(
-          'z-50 text-white rounded-md font-semibold bg-black cursor-pointer capitalize',
-          labelSizeClasses
-        )}
-        onClick={toggleSwitcher}
-      >
-        {query.slug[0].replace(/\-/g, ' ')}
-      </div>
-    </>
+      </Popover.Panel>
+    </Popover>
   )
 }
---
+++ data/libraries.js
@@ -0,0 +1,49 @@
+export const data = [
+  {
+    id: 'react-three-fiber',
+    label: 'React Three Fiber',
+    github: 'https://github.com/pmndrs/react-three-fiber',
+    description: 'React-three-fiber is a React renderer for three.js.',
+  },
+  {
+    id: 'react-spring',
+    label: 'React Spring',
+    github: 'https://github.com/pmndrs/react-spring',
+    description: 'Bring your components to life with simple spring animation primitives for React',
+  },
+  {
+    id: 'drei',
+    label: 'Drei',
+    github: 'https://github.com/pmndrs/drei',
+    description:
+      'Drei is a growing collection of useful helpers and abstractions for react-three-fiber.',
+  },
+  {
+    id: 'zustand',
+    label: 'Zustand',
+    github: 'https://github.com/pmndrs/zustand',
+    description:
+      'Zustand is a small, fast and scalable bearbones state-management solution, it has a comfy api based on hooks',
+    image: 'zustand-bear.png',
+  },
+  {
+    id: 'jotai',
+    label: 'Jotai',
+    github: 'https://github.com/pmndrs/jotai',
+    description:
+      'Jotai is a primitive and flexible yet spooky small state management library for React.',
+  },
+  {
+    id: 'a11y',
+    label: 'A11y',
+    github: 'https://github.com/pmndrs/react-three-a11y',
+    description:
+      '@react-three/a11y brings accessibility to webGL with easy-to-use react-three-fiber components',
+  },
+  {
+    id: 'react-postprocessing',
+    label: 'React Postprocessing',
+    github: 'https://github.com/pmndrs/react-postprocessing',
+    description: 'React Postprocessing is a postprocessing wrapper for @react-three/fiber',
+  },
+]
