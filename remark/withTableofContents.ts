1bb228be8ac20a25a90355a8ebe6af00c48f12b0
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-06-25 10:40:33 -0500

    fix: parse snippets in headings, fix associated links

--- remark/withTableofContents.ts
+++ remark/withTableofContents.ts
@@ -10,15 +10,23 @@ const withTableofContents = (toc?: any[]) => {
     for (let i = 0; i < tree.children.length; i++) {
       const node = tree.children[i]
       if (node.type === 'heading' && [2, 3].includes(node.depth)) {
-        const title = node.children
-          .filter((n) => n.type === 'text')
-          .map((n) => n.value)
-          .join('')
+        const children = node.children.filter((n) => ['text', 'inlineCode'].includes(n.type))
 
+        const title = children
+          .map((n) =>
+            n.type === 'inlineCode'
+              ? // Cleanup links for code-only titles
+                n.value.replace(/^(get|set)\s|\(.+|\??\:.+/, '')
+              : n.value
+          )
+          .join('')
         const slug = slugify(title)
 
+        const content = children
+          .map((n) => (n.type === 'text' ? n.value : `<${n.type}>{'${n.value}'}</${n.type}>`))
+          .join('')
         node.type = 'jsx'
-        node.value = `<Heading id={"${slug}"} level={${node.depth}}>${title}</Heading>`
+        node.value = `<Heading id={"${slug}"} level={${node.depth}}>${content}</Heading>`
 
         if (Array.isArray(toc)) {
           toc.push({ slug, title, depth: node.depth })
