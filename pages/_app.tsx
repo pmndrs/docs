4861244524c7b07c8d0091f9ccbf72ede3913452
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-09-12 23:02:10 -0500

    chore: add missing fonts, dedup italics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,7 +35,6 @@ function App({ Component, pageProps }: AppProps) {
         <meta property="og:type" content="website" />
 
         <link rel="preload" href="/fonts/inter-var.woff2" as="font" crossOrigin="" />
-        <link rel="preload" href="/fonts/inter-italic-var.woff2" as="font" crossOrigin="" />
         <link rel="preload" href="/fonts/meslo.woff2" as="font" crossOrigin="" />
       </Head>
       <Component {...pageProps} />
