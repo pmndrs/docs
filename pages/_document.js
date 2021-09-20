c886f3e7a6a184a03a7938005d1399da05dd5769
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-09-20 08:10:47 -0500

    chore(master): cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -14,13 +14,12 @@ export default class Doc extends Document {
           <link rel="canonical" href="https://pmnd.rs/" />
           <meta name="googlebot" content="follow, index, noarchive" />
           <meta name="robots" content="follow, index, noarchive" />
-          <meta name="viewport" content="initial-scale=1,width=device-width" />
 
-          <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 
-          <link rel="manifest" href="./site.webmanifest" />
+          <link rel="manifest" href="/site.webmanifest" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="#000000" />
           <meta name="apple-mobile-web-app-title" content="PMNDRS" />
