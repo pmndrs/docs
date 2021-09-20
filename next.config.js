013359bf7748c4c53f4901fb934871bdf8958e6e
Cody Bennett <hi@codyb.co>
2021-09-20 08:53:51 -0500

    chore(master): cleanup (#178)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   crossOrigin: 'anonymous',
   pwa: {
+    disable: process.env.NODE_ENV === 'development',
     dest: 'public',
   },
 });
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import '../styles/index.scss';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1,width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
