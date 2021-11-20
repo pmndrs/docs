5626720f4261f213d69ec86845b49b5d1c157dbc
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-11-20 09:53:22 -0600

    feat(docs): fetch remote docs from GitHub

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,9 +1,6 @@
-import fs from 'fs'
 import matter from 'gray-matter'
 import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
-import path from 'path'
-import { getDocsPaths, getAllDocs, DOCS_PATH } from 'utils/mdxUtils'
 import Layout from 'components/Layout'
 import components from 'components/mdx'
 import Seo from 'components/Seo'
@@ -14,8 +11,9 @@ import setValue from 'set-value'
 import { useRouter } from 'next/router'
 import { useDocs } from 'store/docs'
 import { useEffect } from 'react'
+// import { data } from 'data/libraries'
 
-export default function PostPage({ toc, source, allDocs, nav, frontMatter }) {
+export default function PostPage({ toc, source, allDocs, nav, title }) {
   const { query } = useRouter()
   const { setDocs, setCurrentDocs } = useDocs()
   const name = query.slug[0]
@@ -29,22 +27,9 @@ export default function PostPage({ toc, source, allDocs, nav, frontMatter }) {
     <Layout nav={nav} toc={toc}>
       <Seo name={name} />
       <main className="max-w-3xl mx-auto">
-        {frontMatter.title && (
-          <div className="pb-6 mb-4 border-b post-header">
-            <h1 className="mb-4 text-5xl font-bold tracking-tighter">{frontMatter.title}</h1>
-            {frontMatter.description && (
-              <MDXRemote
-                {...frontMatter.description}
-                components={{
-                  ...components,
-                  p: ({ children }) => (
-                    <p className="text-base leading-4 text-gray-400 leading-5">{children}</p>
-                  ),
-                }}
-              />
-            )}
-          </div>
-        )}
+        <div className="pb-6 mb-4 border-b post-header">
+          <h1 className="mb-4 text-5xl font-bold tracking-tighter">{title}</h1>
+        </div>
         <main className="content-container">
           <MDXRemote {...source} components={components} />
         </main>
@@ -53,14 +38,90 @@ export default function PostPage({ toc, source, allDocs, nav, frontMatter }) {
   )
 }
 
+// const settings = data.reduce(
+//   (acc, { id, dir = 'markdown' }) => ({
+//     ...acc,
+//     [id]: {
+//       dir,
+//     },
+//   }),
+//   {}
+// )
+
+const settings: { [key: string]: { dir: string; tag?: string } } = {
+  'react-three-fiber': {
+    dir: 'markdown',
+  },
+}
+
+const cachedPages = new Map()
+
+const getPages = async (lib: keyof typeof settings) => {
+  const cached = cachedPages.get(lib)
+  if (cached) return cached
+
+  const { dir, tag = 'master' } = settings[lib]
+
+  const { tree } = await fetch(
+    `https://api.github.com/repos/pmndrs/${lib}/git/trees/${tag}?recursive=1`
+  ).then((res) => res.json())
+
+  const isMarkdown = ({ path }) => path.startsWith(`${dir}/`) && /\.mdx?$/.test(path)
+
+  const pages = await Promise.all(
+    tree.filter(isMarkdown).map(async ({ path }) => {
+      const postData = await fetch(
+        `https://raw.githubusercontent.com/pmndrs/${lib}/${tag}/${path}`
+      ).then((res) => res.text())
+
+      const localPath = path
+        .toLowerCase()
+        .replace(`${dir}/`, '')
+        .replace(/\.mdx?$/, '')
+
+      const slug = [lib, ...localPath.split('/')]
+
+      const url = slug.join('/')
+      const title = slug[slug.length - 1]
+
+      return { postData, slug, url, title }
+    })
+  )
+
+  cachedPages.set(lib, pages)
+
+  return pages
+}
+
+const getAllPages = async () => {
+  const libs = Object.keys(settings)
+  const pages = await Promise.all(libs.map(getPages))
+
+  return pages.flat()
+}
+
+export const getStaticPaths = async () => {
+  const pages = await getAllPages()
+  const paths = pages.map(({ slug }) => ({ params: { slug } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
 export const getStaticProps = async ({ params }) => {
-  const postFilePath = path.join(DOCS_PATH, `${path.join(...params.slug)}.mdx`)
-  const postData = fs.readFileSync(postFilePath)
+  const [lib] = params.slug
+  const pages = await getPages(lib)
+
+  const post = pages.find((page) => page.slug.join('/') === params.slug.join('/'))
+  const { postData, title } = post
+
   const { content, data } = matter(postData)
-  const allDocs = await getAllDocs()
+  const allDocs = await getAllPages()
 
   const nav = allDocs.reduce((nav, file) => {
-    const [lib, ...rest] = file.url.split('/').filter(Boolean)
+    const [lib, ...rest] = file.slug
     const _path = `${lib}${rest.length === 1 ? '..' : '.'}${rest.join('.')}`
     setValue(nav, _path, file)
     return nav
@@ -76,31 +137,13 @@ export const getStaticProps = async ({ params }) => {
     scope: data,
   })
 
-  // Also serialize descriptions
-  const description = data.description ? await serialize(data.description) : null
-  const frontMatter = { ...data, description }
-
   return {
     props: {
       allDocs,
       nav,
       toc,
       source,
-      frontMatter,
+      title,
     },
-  }
-}
-
-export const getStaticPaths = async () => {
-  // Map the path into the static paths object required by Next.js
-  const paths = (await getDocsPaths()).map((slug) => ({
-    params: {
-      slug: slug.split(path.sep).filter(Boolean),
-    },
-  }))
-
-  return {
-    paths,
-    fallback: false,
   }
 }
---
+++ pages/react-three-fiber/deprecated/index.tsx
@@ -0,0 +1,3 @@
+import redirect from '../../../utils/redirect'
+
+export default redirect('/react-three-fiber/deprecated/api-v5')
