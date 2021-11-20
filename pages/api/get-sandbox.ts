64957e02a0b208144a736010c588e6ea39045316
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-11-20 03:08:58 -0600

    types(api): fix deployment

--- pages/api/get-sandbox.ts
+++ pages/api/get-sandbox.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 export default async function handler({ query: { id } }, res) {
   const data = await fetch(`https://codesandbox.io/api/v1/sandboxes/${id}`)
     .then((rsp) => rsp.json())
-    .then((rsp) => rsp.data)
+    .then((rsp: any) => rsp.data)
 
   res.status(200).json(data)
 }
