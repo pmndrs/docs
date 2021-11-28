b09ae0bc3952ddb2ecdc58a4cdf27af9b60f747e
Cody Bennett <hi@codyb.co>
2021-11-28 17:35:31 -0600

    chore: cleanup utils (#210)

--- utils/titleCase.ts
+++
@@ -1,7 +0,0 @@
-export default function titleCase(str: string) {
-  var splitStr = str.toLowerCase().split(' ')
-  for (var i = 0; i < splitStr.length; i++) {
-    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1)
-  }
-  return splitStr.join(' ')
-}
