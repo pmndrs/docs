ec343130f4b8fffe20c029a7f53c51a5b20cdc2c
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-11-28 14:36:50 -0600

    Merge branch 'docs' into fetch-remote

--- pages/api/get-sandbox.ts
+++ pages/api/get-sandbox.ts
@@ -1,9 +1,23 @@
 import fetch from 'node-fetch'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler({ query: { id } }, res) {
-  const data = await fetch(`https://codesandbox.io/api/v1/sandboxes/${id}`)
-    .then((rsp) => rsp.json())
-    .then((rsp: any) => rsp.data)
+export interface ICodesandbox {
+  title: string
+  description: string
+  alias: string
+  screenshot_url: string
+  tags: string[]
+  modules: { directory_shortid: string; title: string; code: string }[]
+  directories: { shortid: string; title: string }[]
+}
 
-  res.status(200).json(data)
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { query } = req
+  const { id } = query
+
+  const { data }: { data?: ICodesandbox } = await fetch(
+    `https://codesandbox.io/api/v1/sandboxes/${id}`
+  ).then((rsp) => rsp.json())
+
+  return res.status(200).json(data)
 }
