84f358c1e1c192fb151738723468d1be9b1edab5
Cody Bennett <hi@codyb.co>
2021-12-03 08:00:22 -0600

    chore(mdx): remove duplicate InlineCode & cleanup R3F transclusion (#214)

--- components/mdx/index.tsx
+++ components/mdx/index.tsx
@@ -8,14 +8,6 @@ import { GridUsedBy } from './GridUsedBy'
 import { Demo } from './Demo'
 import { DemoGrid } from './DemoGrid'
 
-const InlineCode = ({ children }) => {
-  return (
-    <code className="px-2 py-1 rounded font-mono text-sm text-gray-800 bg-gray-100">
-      {children}
-    </code>
-  )
-}
-
 const components = {
   StoryBook,
   Demo,
@@ -57,6 +49,7 @@ const components = {
 
     const clampedLevel = Math.min(Math.max(level, 2), 4)
     const Comp = `h${clampedLevel}`
+
     return (
       <a
         href={`#${id}`}
@@ -71,34 +64,36 @@ const components = {
   ul: ({ children }) => <ul className="px-4 mb-8">{children}</ul>,
   ol: ({ children }) => <ol className="px-4 mb-8">{children}</ol>,
   li: ({ children }) => <li className="mb-4 text-base leading-6 text-gray-700">{children}</li>,
-  inlineCode: InlineCode,
-  InlineCode,
+  inlineCode: ({ children }) => (
+    <code className="px-2 py-1 rounded font-mono text-sm text-gray-800 bg-gray-100">
+      {children}
+    </code>
+  ),
   p: ({ children }) => <p className="mb-4 text-base text-gray-700">{children}</p>,
   blockquote: ({ children }) => (
     <blockquote className="mb-8 text-base pl-4 border-l-4 border-gray-600">{children}</blockquote>
   ),
-  table: ({ children }) => {
-    return (
-      <div className="flex flex-col my-6">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-6 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow-lg overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="divide-y divide-gray-200 w-full"> {children}</table>
-            </div>
+  table: ({ children }) => (
+    <div className="flex flex-col my-6">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-6 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow-lg overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="divide-y divide-gray-200 w-full"> {children}</table>
           </div>
         </div>
       </div>
-    )
-  },
-  a: (props) => {
-    if (props.href.startsWith('https://')) {
+    </div>
+  ),
+  a: ({ href, children }) => {
+    if (href.startsWith('https://')) {
       return (
-        <a href={props.href} target="_blank" rel="noopener noreferrer">
-          {props.children}
+        <a href={href} target="_blank" rel="noopener noreferrer">
+          {children}
         </a>
       )
     }
-    return <a href={props.href}>{props.children}</a>
+
+    return <a href={href}>{children}</a>
   },
 }
 
