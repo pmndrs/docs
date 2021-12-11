d5d10748b78b5f8b7ac2a695239820fa789d0cce
Cody Bennett <hi@codyb.co>
2021-12-11 04:52:33 -0600

    feat: use VSCode theme, prism => shiki (#218)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import { AppProps } from 'next/app'
 import '../css/main.css'
-import '../css/pmndrs.css'
 
 function App({ Component, pageProps }: AppProps) {
   return (
