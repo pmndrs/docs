513520ab5d60983f3b2d166440078686f5b9617c
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-03-20 01:19:03 -0500

    fix(Nav): link shallow categories

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,6 +3,24 @@ import { useRouter } from 'next/router'
 import clsx from 'clsx'
 import Link from 'next/link'
 import useDocs from 'hooks/useDocs'
+import type { Doc } from 'utils/docs'
+
+function NavItem({ doc, active, ...props }) {
+  return (
+    <li {...props}>
+      <Link href={doc.url ?? '/'}>
+        <a
+          className={clsx(
+            'rounded-md block px-6 py-2 text-gray-800 font-normal hover:bg-gray-50 cursor-pointer',
+            active && 'bg-gray-100'
+          )}
+        >
+          {doc.title}
+        </a>
+      </Link>
+    </li>
+  )
+}
 
 function Nav() {
   const { asPath } = useRouter()
@@ -25,25 +43,18 @@ function Nav() {
 
   return (
     <ul>
-      {Object.entries(nav).map(([key, children]) => (
+      {Object.entries(nav).map(([key, doc]) => (
         <Fragment key={key}>
           <h3 className="px-6 mt-8 mb-2 text-sm lg:text-xs text-gray-900 uppercase tracking-wide font-semibold">
             {key.replace(/\-/g, ' ')}
           </h3>
-          {Object.entries(children).map(([key, doc]) => (
-            <li key={key}>
-              <Link href={doc.url}>
-                <a
-                  className={clsx(
-                    'rounded-md block px-6 py-2 text-gray-800 font-normal hover:bg-gray-50 cursor-pointer',
-                    doc.url === asPath && 'bg-gray-100'
-                  )}
-                >
-                  {doc.title}
-                </a>
-              </Link>
-            </li>
-          ))}
+          {(doc as Doc).url ? (
+            <NavItem key={key} active={(doc as Doc).url === asPath} doc={doc} />
+          ) : (
+            Object.entries(doc).map(([key, doc]) => (
+              <NavItem key={key} active={doc.url === asPath} doc={doc} />
+            ))
+          )}
         </Fragment>
       ))}
     </ul>
