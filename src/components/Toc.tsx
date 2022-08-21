2896281569394bb3d5ee696b7a94f5b66d532e9b
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-08-21 11:57:47 -0500

    fix: disable toc for now due to SSR

--- src/components/Toc.tsx
+++ src/components/Toc.tsx
@@ -9,19 +9,19 @@ export interface ToCProps {
 function Toc({ toc }: ToCProps) {
   const [activeIndex, setActiveIndex] = React.useState(0)
 
-  React.useEffect(() => {
-    const headings = toc.map(({ id }) => document.getElementById(id))
+  // React.useEffect(() => {
+  //   const headings = toc.map(({ id }) => document.getElementById(id))
 
-    const observer = new IntersectionObserver(([entry]) => {
-      if (entry.intersectionRatio > 0) {
-        const headingIndex = headings.indexOf(entry.target as HTMLElement)
-        setActiveIndex(headingIndex)
-      }
-    })
-    headings.forEach((element) => observer.observe(element as Element))
+  //   const observer = new IntersectionObserver(([entry]) => {
+  //     if (entry.intersectionRatio > 0) {
+  //       const headingIndex = headings.indexOf(entry.target as HTMLElement)
+  //       setActiveIndex(headingIndex)
+  //     }
+  //   })
+  //   headings.forEach((element) => observer.observe(element as Element))
 
-    return () => observer.disconnect()
-  }, [toc])
+  //   return () => observer.disconnect()
+  // }, [toc])
 
   return (
     <div className="flex flex-col justify-between overflow-y-auto sticky max-h-(screen-16) pb-6 top-16">
