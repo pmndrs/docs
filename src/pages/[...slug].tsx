e126ecf89cb64e90393a9159a223d1ec38a04a82
Cody Bennett <hi@codyb.co>
2022-11-03 06:14:38 -0500

    experiment: full static builds (#261)

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useRouter } from 'next/router'
 import type { GetStaticProps } from 'next'
 import type libs from 'data/libraries'
 import Layout from 'components/Layout'
@@ -9,12 +10,20 @@ import { type CSB, CSBContext, fetchCSB } from 'hooks/useCSB'
 import { getDocs } from 'utils/docs'
 
 export interface PostPageProps {
+  redirect: boolean
   docs: Doc[]
   doc: Doc
   boxes: Record<string, CSB>
 }
 
-export default function PostPage({ docs, doc, boxes }: PostPageProps) {
+export default function PostPage({ redirect, docs, doc, boxes }: PostPageProps) {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    if (redirect) router.replace(doc.url, undefined, { shallow: true })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <DocsContext.Provider value={docs}>
       <CSBContext.Provider value={boxes}>
@@ -45,29 +54,49 @@ export const getStaticProps: GetStaticProps<PostPageProps> = async ({ params }) 
   if (!docs?.length) return { notFound: true }
 
   const url = `/${slug.join('/')}`.toLowerCase()
-  const doc = docs.find((doc) => doc.url === url)
+  let doc = docs.find((doc) => doc.url === url)
 
+  // Handle alternate routes
+  let redirect = !doc
   if (!doc) {
-    const alternate = docs.find((doc) => doc.url.startsWith(url))
-    return alternate
-      ? { redirect: { permanent: false, destination: alternate.url } }
-      : { notFound: true }
+    doc = docs.find((doc) => doc.url.startsWith(url))
+    if (!doc) return { notFound: true }
   }
 
   const boxes = await fetchCSB(docs.flatMap((doc) => doc.boxes))
 
   return {
     props: {
+      redirect,
       // Don't send other pages' source blobs
       docs: docs.map(({ source, ...rest }) => ({ ...rest, source: null! })),
       doc,
       boxes,
     },
-    revalidate: 300,
   }
 }
 
 export const getStaticPaths = async () => {
-  const paths = (await getDocs()).map(({ slug }) => ({ params: { slug } }))
-  return { paths, fallback: 'blocking' }
+  const docs = await getDocs()
+
+  const libs = new Set<string>()
+  const categories = new Set<string>(['/'])
+  const paths = docs.map(({ slug }) => ({ params: { slug } }))
+
+  for (const doc of docs) {
+    const [lib, category, page] = doc.slug
+
+    if (!libs.has(lib)) {
+      libs.add(lib)
+      paths.push({ params: { slug: [lib] } })
+    }
+
+    const url = `${lib}/${category}`
+    if (page && !categories.has(url)) {
+      categories.add(url)
+      paths.push({ params: { slug: [lib, category] } })
+    }
+  }
+
+  return { paths, fallback: false }
 }
