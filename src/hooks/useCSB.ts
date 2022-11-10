3f57eb0a33bdf1b985cac54ecc1d2c7116e0b8ab
“Isaac <isaac@isaacmason.com>
2022-11-10 20:05:03 +1000

    fix(useCSB): add delay between codesandbox api requests to avoid rate limits

--- src/hooks/useCSB.ts
+++ src/hooks/useCSB.ts
@@ -10,11 +10,16 @@ export interface CSB {
 
 export const CSBContext = React.createContext<Record<string, CSB>>({})
 
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 export async function fetchCSB(ids: string[]) {
   const boxes: Record<string, CSB> = {}
 
   for (const id of ids) {
     try {
+      // add a delay between requests to avoid being rate limited
+      await wait(100)
+
       const { title, description, alias, screenshot_url, tags } = await fetch(
         `https://codesandbox.io/api/v1/sandboxes/${id}`
       ).then(async (res) => (await res.json()).data)
