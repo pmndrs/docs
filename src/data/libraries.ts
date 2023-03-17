8d749aed57bf8edd66d06166b8fde0be9a9c6238
Cody Bennett <hi@codyb.co>
2023-03-17 13:12:28 -0500

    chore: fix CI

--- src/data/libraries.ts
+++ src/data/libraries.ts
@@ -79,7 +79,7 @@ const libraries: Record<string, Library> = {
     github: 'https://github.com/pmndrs/react-postprocessing',
     description: 'React Postprocessing is a postprocessing wrapper for @react-three/fiber',
     image: reactPostprocessingShare.src,
-    docs: 'CodyJasonBennett/react-postprocessing/migrate-docs/docs',
+    docs: 'CodyJasonBennett/react-postprocessing/master/docs',
   },
 }
 
