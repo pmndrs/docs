35f950588fbd8336062858b943ffcae42add6a9e
Ali <ali.hussein.pre@gmail.com>
2023-11-30 22:03:09 +0100

    feat: persist theme preferences

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,29 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import cookie from 'cookiejs'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-
         <link rel="preload" href="/fonts/inter-regular.woff2" as="font" crossOrigin="true" />
         <link rel="preload" href="/fonts/inter-medium.woff2" as="font" crossOrigin="true" />
         <link rel="preload" href="/fonts/inter-semibold.woff2" as="font" crossOrigin="true" />
         <link rel="preload" href="/fonts/inter-bold.woff2" as="font" crossOrigin="true" />
         <link rel="preload" href="/fonts/meslo.woff2" as="font" crossOrigin="" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                try {
+                  if (localStorage.theme === 'dark') {
+                    document.documentElement.classList.add('dark')
+                  } else {
+                    document.documentElement.classList.remove('dark')
+                  }
+                } catch (_) {}
+              `,
+          }}
+        />
       </Head>
       <body tabIndex={-1}>
         <Main />
