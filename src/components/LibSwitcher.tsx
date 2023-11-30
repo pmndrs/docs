d6bf6ef87d3cf827cebb19851801853fbe411134
Ali <ali.hussein.pre@gmail.com>
2023-11-30 22:47:20 +0100

    style: add dark mode to lib switcher

--- src/components/LibSwitcher.tsx
+++ src/components/LibSwitcher.tsx
@@ -12,16 +12,16 @@ export default function LibSwitcher() {
 
   return (
     <Popover className="relative mt-4">
-      <Popover.Button className="block w-full px-6 py-2 focus:outline-none bg-black rounded-md font-bold text-lg text-white">
+      <Popover.Button className="block w-full px-6 py-2 focus:outline-none bg-black rounded-md font-bold text-lg text-white dark:bg-white dark:text-gray-900">
         {currentPage}
       </Popover.Button>
-      <Popover.Panel className="absolute z-10 w-full mt-4 p-3 shadow-2xl bg-white rounded-md">
+      <Popover.Panel className="absolute z-10 w-full mt-4 p-3 shadow-2xl bg-white dark:bg-gray-800 rounded-md">
         <div className="flex flex-col space-y-3">
           {Object.entries(libs).map(([id, data]) => (
             <Link key={id} href={data.url}>
               <a
                 className={clsx(
-                  'px-3 py-2 hover:bg-gray-50 rounded-md font-normal text-base',
+                  'px-3 py-2 hover:bg-gray-50 rounded-md font-normal text-base dark:hover:bg-gray-600/30',
                   id === query.slug![0] && 'sr-only'
                 )}
               >
