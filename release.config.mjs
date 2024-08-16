8f2e80e96b6c856eb7fb35cb8a51a35011aaf3cd
Antoine BERNIER <antoine.bernier@gmail.com>
2024-08-16 14:26:16 +0200

    fix: default config

--- release.config.mjs
+++ release.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('semantic-release').GlobalConfig} */
-const config = {
-  branches: [
-    // default:
-    '+([0-9])?(.{+([0-9]),x}).x',
-    'master',
-    'main',
-    'next',
-    'next-major',
-    { name: 'beta', prerelease: true },
-    { name: 'alpha', prerelease: true },
-    // added:
-    { name: 'canary-*', prerelease: true, channel: 'canary' },
-  ],
-}
+const config = {}
 
 export default config
