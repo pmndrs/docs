53d0212e10e9a20b2222673fef7d41d3803eae4f
Antoine BERNIER <antoine.bernier@gmail.com>
2024-08-18 08:37:01 +0200

    husky + lint-staged

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config
