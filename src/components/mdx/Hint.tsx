104c437ec4f83e2404f6d2428df5c544f00c8589
Antoine BERNIER <antoine.bernier@gmail.com>
2024-08-20 18:41:47 +0200

    note icon

--- src/components/mdx/Hint.tsx
+++ src/components/mdx/Hint.tsx
@@ -1,4 +1,5 @@
 import cn from '@/lib/cn'
+import { IoDocumentTextOutline } from 'react-icons/io5'
 
 export const Hint = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -9,6 +10,10 @@ export const Hint = ({ children }: { children: React.ReactNode }) => {
         'bg-hint-container',
       )}
     >
+      <p className="mb-2 flex items-center gap-1 text-lg">
+        <IoDocumentTextOutline className="text-[125%]" />
+        Note
+      </p>
       {children}
     </div>
   )
