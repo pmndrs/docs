4b3207ff313a4019832d59ef58b8e57c8ad2402e
Antoine BERNIER <antoine.bernier@gmail.com>
2024-08-23 10:28:36 +0200

    chore: deprecated Hint

--- src/components/mdx/Hint.tsx
+++ src/components/mdx/Hint.tsx
@@ -1,5 +1,11 @@
 import { Gha } from './Gha'
 
+/**
+ *
+ * @deprecated
+ */
 export const Hint = ({ children }: { children: React.ReactNode }) => {
+  console.warn('Hint is deprecated, use Gha instead.')
+
   return <Gha keyword="NOTE">{children}</Gha>
 }
