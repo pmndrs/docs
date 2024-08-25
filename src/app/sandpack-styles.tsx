6a42ca5b370ea4eb154721b84573486e32501214
Antoine BERNIER <antoine.bernier@gmail.com>
2024-08-25 11:32:31 +0200

    SandpackCSS

---
+++ src/app/sandpack-styles.tsx
@@ -0,0 +1,16 @@
+// https://sandpack.codesandbox.io/docs/guides/ssr#nextjs-app-dir
+
+'use client'
+
+import { getSandpackCssText } from '@codesandbox/sandpack-react'
+import { useServerInsertedHTML } from 'next/navigation'
+
+/**
+ * Ensures CSSinJS styles are loaded server side.
+ */
+export const SandpackCSS = () => {
+  useServerInsertedHTML(() => {
+    return <style dangerouslySetInnerHTML={{ __html: getSandpackCssText() }} id="sandpack" />
+  })
+  return null
+}
