9ed6731a5812f1941aa1f34d8b7cd6b60bbbb113
Antoine BERNIER <antoine.bernier@gmail.com>
2024-08-28 12:50:29 +0200

    feat: auto-img [w/h]

--- src/components/mdx/Img/Img.tsx
+++ src/components/mdx/Img/Img.tsx
@@ -1,7 +1,21 @@
 import cn from '@/lib/cn'
 import { ComponentProps } from 'react'
 
-export function Img({ src, alt = '', className, ...props }: ComponentProps<'img'>) {
+import sizeOf from 'image-size'
+import { resolve } from 'path'
+
+export async function Img({ src, alt = '', className, ...props }: ComponentProps<'img'>) {
+  const dims: Partial<Pick<ComponentProps<'img'>, 'width' | 'height'>> = {
+    width: undefined,
+    height: undefined,
+  }
+  if (process.env.MDX_BASEURL && src?.startsWith(process.env.MDX_BASEURL)) {
+    const path = resolve(src.replace(process.env.MDX_BASEURL, process.env.MDX!))
+    const { width, height } = sizeOf(path)
+    dims.width = width
+    dims.height = height
+  }
+
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
@@ -10,6 +24,7 @@ export function Img({ src, alt = '', className, ...props }: ComponentProps<'img'
       loading="lazy"
       alt={alt}
       className={cn('bg-surface-container inline-block rounded-lg', className)}
+      {...dims}
       {...props}
     />
   )
