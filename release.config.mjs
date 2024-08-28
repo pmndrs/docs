f964d9f637c87367aac76ee5e704ae3daa5e1699
Antoine BERNIER <antoine.bernier@gmail.com>
2024-08-28 09:34:11 +0200

    fix: sr

--- release.config.mjs
+++ release.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('semantic-release').GlobalConfig} */
-const config = {}
+const config = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      // https://github.com/semantic-release/semantic-release/issues/2204#issuecomment-2154938064
+      {
+        successComment: false,
+        failTitle: false,
+      },
+    ],
+  ],
+}
 
 export default config
