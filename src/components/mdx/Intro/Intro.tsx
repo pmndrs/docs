f9eacd66c55b05f45ea425d36b6cf4831b58d0f6
Antoine BERNIER <antoine.bernier@gmail.com>
2024-09-14 20:44:19 +0200

    fix: semantic tags

--- src/components/mdx/Intro/Intro.tsx
+++ src/components/mdx/Intro/Intro.tsx
@@ -1,6 +1,14 @@
 import cn from '@/lib/cn'
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
 import { ComponentProps } from 'react'
 
-export function Intro({ className, ...props }: ComponentProps<'div'>) {
-  return <div {...props} className={cn(className, 'my-6 text-xl leading-relaxed')} />
+export function Intro({ children, className, ...props }: ComponentProps<'div'>) {
+  return (
+    <section {...props} className={cn(className, 'my-6 text-xl leading-relaxed')}>
+      <VisuallyHidden>
+        <h2>Summary</h2>
+      </VisuallyHidden>
+      {children}
+    </section>
+  )
 }
