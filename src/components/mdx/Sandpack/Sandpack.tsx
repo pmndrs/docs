e275e69eaa0bd2b05705750cbe3e1f3f8900a75f
Antoine BERNIER <antoine.bernier@gmail.com>
2024-10-31 22:07:08 +0100

    feat(Sandpack): individual files options

--- src/components/mdx/Sandpack/Sandpack.tsx
+++ src/components/mdx/Sandpack/Sandpack.tsx
@@ -1,9 +1,15 @@
 import cn from '@/lib/cn'
 import { crawl } from '@/utils/docs'
-import { Sandpack as SP } from '@codesandbox/sandpack-react'
+import {
+  Sandpack as SP,
+  type SandpackFile,
+  type SandpackFiles,
+  type SandpackProps,
+} from '@codesandbox/sandpack-react'
 import fs from 'node:fs'
 import path from 'node:path'
-import { ComponentProps } from 'react'
+
+type Files = Record<string, Omit<SandpackFile, 'code'>>
 
 function getSandpackDependencies(folder: string) {
   const pkgPath = `${folder}/package.json`
@@ -13,9 +19,11 @@ function getSandpackDependencies(folder: string) {
   return JSON.parse(str).dependencies as Record<string, string>
 }
 
-type File = { code: string }
-
-async function getSandpackFiles(folder: string, extensions = ['js', 'ts', 'jsx', 'tsx', 'css']) {
+async function getSandpackFiles(
+  folder: string,
+  files: Files = {},
+  extensions = ['js', 'ts', 'jsx', 'tsx', 'css'],
+) {
   const filepaths = await crawl(
     folder,
     (dir) =>
@@ -23,32 +31,36 @@ async function getSandpackFiles(folder: string, extensions = ['js', 'ts', 'jsx',
   )
   // console.log('filepaths', filepaths)
 
-  const files = filepaths.reduce(
-    (acc, filepath) => {
-      const relativeFilepath = path.relative(folder, filepath)
+  return filepaths.reduce((acc, filepath) => {
+    const relativeFilepath = path.relative(folder, filepath)
 
-      return {
-        ...acc,
-        [`/${relativeFilepath}`]: {
-          code: fs.readFileSync(filepath, 'utf-8'),
-        },
-      }
-    },
-    {} as Record<string, File>,
-  )
-
-  return files
+    const key = `/${relativeFilepath}`
+    const file = files[key]
+    return {
+      ...acc,
+      [key]: {
+        ...file,
+        code: fs.readFileSync(filepath, 'utf-8'),
+      },
+    }
+  }, {} as SandpackFiles)
 }
 
 // https://sandpack.codesandbox.io/docs/getting-started/usage
 export const Sandpack = async ({
   className,
   folder,
+  files,
   ...props
-}: { className: string; folder?: string } & ComponentProps<typeof SP>) => {
+}: {
+  className: string
+  folder?: string
+  files?: Files
+} & Omit<SandpackProps, 'files'>) => {
   // console.log('folder', folder)
 
-  const files = folder ? await getSandpackFiles(folder) : props.files
+  const _files = folder ? await getSandpackFiles(folder, files) : files
+  // console.log('_files', _files)
 
   const pkgDeps = folder ? getSandpackDependencies(folder) : null
   const dependencies = pkgDeps ?? props.customSetup?.dependencies
@@ -56,6 +68,7 @@ export const Sandpack = async ({
     ...props.customSetup,
     dependencies,
   }
+  // console.log('customSetup', customSetup)
 
   const options = {
     ...props.options,
@@ -64,7 +77,7 @@ export const Sandpack = async ({
 
   return (
     <div className={cn(className, 'sandpack')}>
-      <SP {...props} files={files} customSetup={customSetup} options={options} />
+      <SP {...props} files={_files} customSetup={customSetup} options={options} />
     </div>
   )
 }
