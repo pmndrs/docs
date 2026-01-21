c8b2644d6684cba50ae00ae1142d1c9d1ea38853
Antoine BERNIER <antoine.bernier@gmail.com>
2026-01-21 13:06:12 +0100

    snapshotDir

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, devices } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './src/app',
+  snapshotDir: './test-results',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
