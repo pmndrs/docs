c5215f8e21b52ba77a8d89d0c0ef6eb82c0f367d
Antoine BERNIER <antoine.bernier@gmail.com>
2026-01-22 08:41:06 +0100

    dark

--- src/app/[...slug]/page.test.ts
+++ src/app/[...slug]/page.test.ts
@@ -8,6 +8,10 @@ test('/getting-started/introduction', async ({ page }) => {
   await page.goto('/getting-started/introduction')
   await page.waitForLoadState('networkidle')
   await expect(page).toHaveScreenshot({ fullPage: true })
+  //
+  await page.emulateMedia({ colorScheme: 'dark' })
+  await page.reload()
+  await expect(page).toHaveScreenshot({ fullPage: true })
 })
 
 //
@@ -16,6 +20,10 @@ test('/getting-started/authoring', async ({ page }) => {
   await page.goto('/getting-started/authoring')
   await page.waitForLoadState('networkidle')
   await expect(page).toHaveScreenshot({ fullPage: true })
+  //
+  await page.emulateMedia({ colorScheme: 'dark' })
+  await page.reload()
+  await expect(page).toHaveScreenshot({ fullPage: true })
 })
 
 //
@@ -23,5 +31,9 @@ test('/getting-started/authoring', async ({ page }) => {
 test('/getting-started/github-actions', async ({ page }) => {
   await page.goto('/getting-started/github-actions')
   await page.waitForLoadState('networkidle')
+  await expect(page).toHaveScreenshot({ fullPage: true })
+  //
+  await page.emulateMedia({ colorScheme: 'dark' })
+  await page.reload()
   await expect(page).toHaveScreenshot({ fullPage: true })
 })
--- src/app/page.test.ts
+++ src/app/page.test.ts
@@ -2,6 +2,9 @@ import { test, expect } from '@chromatic-com/playwright'
 
 test('home', async ({ page }) => {
   await page.goto('/')
-  await page.waitForLoadState('networkidle')
+  await expect(page).toHaveScreenshot({ fullPage: true })
+  //
+  await page.emulateMedia({ colorScheme: 'dark' })
+  await page.reload()
   await expect(page).toHaveScreenshot({ fullPage: true })
 })
