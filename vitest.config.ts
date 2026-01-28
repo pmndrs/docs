683c24626b58a2d75a4c62ddec5c5ea9f50a5802
Antoine BERNIER <antoine.bernier@gmail.com>
2026-01-28 19:20:38 +0100

    .

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      // Exclude Playwright tests
-      '**/src/app/**/*.test.{ts,tsx}',
+      '**/page.test.tsx', // exclude Playwright page tests
     ],
   },
 })
-
